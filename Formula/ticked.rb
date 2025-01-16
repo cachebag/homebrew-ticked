f1c02a496c4cded72329f89fbb5cf32468cc52ea
akrm al-hakimi <alhakimiakrmj@gmail.com>
2025-01-16 14:58:09 -0500

    fixed treesitter dependencies

--- Formula/ticked.rb
+++ Formula/ticked.rb
@@ -8,6 +8,7 @@ class Ticked < Formula
   license "MIT"
 
   depends_on "python@3.13"
+  depends_on "gcc"  
 
   resource "aiohappyeyeballs" do
     url "https://files.pythonhosted.org/packages/7f/55/e4373e888fdacb15563ef6fa9fa8c8252476ea071e96fb46defac9f18bf2/aiohappyeyeballs-2.4.4.tar.gz"
